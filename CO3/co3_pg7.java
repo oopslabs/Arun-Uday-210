bbffcdabf6ef5813903ab9e275d30a2e5cc5a870
Arun Uday <72249990+arunuday117@users.noreply.github.com>
2022-05-23 12:15:06 +0530

    23-05-2022

---
+++ CO3/co3_pg7.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.Date;
+public class co3_pg7{
+	public static void main(){
+		
+	}
+}
+interface bill{
+	void struct();
+	void calbill();
+}
+
+class create implements bill{
+	public void struct(){
+		
+	}
+}
