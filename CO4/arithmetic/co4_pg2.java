e4ec04202fdb948bed623b1af4bbb2f9d6f8f569
Arun Uday <72249990+arunuday117@users.noreply.github.com>
2022-06-04 22:00:24 +0530

    4-06-2022

---
+++ CO4/arithmetic/co4_pg2.java
@@ -0,0 +1,17 @@
+package arithmetic;
+import java.util.*;
+
+public class co4_pg2 {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		calculate calculate = new calculate();
+		int i,j;
+		System.out.println("Enter the two numbers");
+		i=sc.nextInt();
+		j=sc.nextInt();
+		calculate.add(i,j);
+		calculate.sub(i,j);
+		calculate.mul(i,j);
+		calculate.div(i,j);
+	}
+}
